832e6ec3f53da21f99322980eb8dab6a5e43a8f3
Lauren Slayman <laurenaslayman@gmail.com>
2023-04-25 12:41:36 -0600

    chore: base

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "/about",
+      element: <About />
+    },
+  ])
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <>
+    Home
+    <br/>
+    <Link to="/about">About</Link>
+    </>
+  )
+}
